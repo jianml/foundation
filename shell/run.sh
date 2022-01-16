fe45dd0efa8dc54a93b23367c47f4c6c15621b74
wujian <792622017@qq.com>
2022-01-16 15:46:21 +0800

    初始化项目

---
+++ shell/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+service="foundation"
+image="jianml/foundation:1.0"
+
+# 校验服务是否存在
+result=$(docker ps -a --format {{.Names}} | grep -E ^${service}$)
+[[ $result == ${service} ]] && docker rm -f ${service} && echo "删除服务[${service}]成功:)"
+
+# 校验镜像是否存在
+result=$(docker images ${image} --format {{.Repository}}:{{.Tag}})
+[[ ${result} == ${image} ]] && docker rmi ${image} && echo "删除镜像[${image}]成功:)"
+
+docker run \
+--restart=always \
+--name ${service} \
+-p 9000:8080 \
+-d ${image}
